ea02087b93bb3f69dae7ecabb46de7a29902dcc4
haidi nata <haidinurhadinata@gmail.com>
2022-04-12 15:29:34 +0800

    mobileService: update itemcontroller

--- services/mobileService/src/controllers/item.controller.ts
+++ services/mobileService/src/controllers/item.controller.ts
@@ -4,7 +4,7 @@ class ItemController {
 
   public getItem = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const data = "item controller"
+      const data: string = "item controller"
 
       res.status(200).json({ data: data, message: 'Success, get item' });
     } catch (error) {
